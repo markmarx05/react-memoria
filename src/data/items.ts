8185abbfbf3f398b5f6960b9561a9af6551114f3
Marcelo Marques <markmarx05@gmail.com>
2022-06-29 07:08:22 -0300

    Adicionando arquivo de configuração

--- src/data/items.ts
+++ src/data/items.ts
@@ -4,6 +4,11 @@ import disney from '../svgs/disney.svg';
 import estrela from '../svgs/estrela.svg';
 import gasolina from '../svgs/gasolina.svg';
 import moto from '../svgs/moto.svg';
+import cao from '../svgs/cao.svg';
+import gato from '../svgs/gato.svg';
+import aviao from '../svgs/aviao.svg';
+import ancora from '../svgs/ancora.svg';
+
 
 
 export const items = [
@@ -12,6 +17,10 @@ export const items = [
     {name: 'disney', icon: disney},
     {name: 'estrela', icon: estrela},
     {name: 'gasolina', icon: gasolina},
-    {name: 'moto', icon: moto}
+    {name: 'moto', icon: moto},
+    {name: 'cao', icon: cao},
+    {name: 'gato', icon: gato},
+    {name: 'aviao', icon: aviao},
+    {name: 'ancora', icon: ancora}
     
 ];
